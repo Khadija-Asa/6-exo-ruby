54f76d83833e0577f483303ded527e8500a4b1ec
khadija.aitsiali <khadija.aitsiali@gmail.com>
2020-11-11 17:12:44 +0100

    add exo 2 ruby

---
+++ exo_2.rb
@@ -0,0 +1,18 @@
+#exo 2 - Table de multiplication
+#L’utilisateur devrait renseigne un nombre de 1 chiffre
+#Le résultat affichera :
+#La table de multiplication du chiffre ##chiffre saisie## est :
+#1 * chiffre saisie = chiffre saisie * 1
+#2 * …
+#3 *
+#…
+#10 *
+
+puts "Saisissez un chiffre"
+
+
+i = 0
+while i < 10
+  i = i + 1
+puts i
+end
