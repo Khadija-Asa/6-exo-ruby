a6fe2f2d15a9e64934aa046af594e73860be1e76
khadija.aitsiali <khadija.aitsiali@gmail.com>
2020-11-16 15:00:04 +0100

    add editing

--- exo_3.rb
+++ exo_3.rb
@@ -3,6 +3,12 @@
 #Bonjour  nom #   si seul le nom est fourni
 #Bonjour Nom # *ET QUE* si les 2 sont fournis
 
+#n = 1
+#puts '#{n}'
+#>> #{n}
+#puts "#{n}"
+#>> 1
+
 puts 'Veuillez entrer votre surnom'
 nickname = gets.chomp
 
@@ -24,4 +30,34 @@ def bonjour (nickname, name)
 		puts 'Bonjour ' + name #on renvoi Bonjour + nom
 
 	end
+end
+
+# > `Hello  surnom`     # si seul le surnom est fourni
+# >
+# > `Bonjour  nom` #   si seul le nom est fourni
+# >
+# > `Bonjour Nom` # **ET QUE** si les 2 sont fournis
+
+
+# celui là il est chiant, c'est l'idée :)
+
+# On demande  à 2 reprises à l'utilisateur de saisir son nom et son surnom
+# Même remarque sur la vérification de la saisie
+
+puts 'Saisir votre nom'
+name = gets.chomp.downcase
+
+puts 'Saisir votre surnom'
+surname = gets.chomp.downcase
+
+# on doit ici faire des gros if, else bien triste 
+# pas de méthode magique
+if name.empty? and !surname.empty?
+  puts "Hello #{surname}"
+elsif !name.empty? and surname.empty?
+  puts "Bonjour #{name}"
+elsif !name.empty? and !surname.empty?
+  puts "Bonjour #{name.capitalize}"
+else
+  puts 'oups tout cassé'
 end
